dad0542d3e7d1c6140294c5ac680712da52f1f72
Raby Yuson <raby.yuson@firstlook.org>
2021-05-11 22:27:19 -0400

    Update day->month

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -3,14 +3,14 @@ import './index.scss'
 const Calendar = () => {
   const now = new Date()
   const today = {
-    day: now.toLocaleString('en-US', { month: 'short' }),
+    month: now.toLocaleString('en-US', { month: 'short' }),
     date: now.getDate()
   }
 
   return (
     <div className="Calendar">
       <div className="Calendar-icon">
-        <div className="Calendar-icon-day">{today.day}</div>
+        <div className="Calendar-icon-month">{today.month}</div>
         <div className="Calendar-icon-date">{today.date}</div>
       </div>
       <div className="Calendar-label">Today</div>
